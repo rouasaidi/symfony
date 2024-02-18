1b301cd33bbb25e4af2171344ffac0a20fcec47b
Aziz <kraiem.medaziz@esprit.tn>
2024-02-19 00:26:23 +0100

    showevents template

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Event;
 use App\Form\EventType;
+use App\Repository\EventRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
@@ -25,11 +26,13 @@ class EventController extends AbstractController
     public function addevent(EventType $f, ManagerRegistry $em,Request $req ): Response
     {
         $Event1 = new Event();
+        $Event1->setImage("default");
 $em=$em->getManager();
 $f=$this->createForm(EventType::class,$Event1);
 $f->handleRequest($req);
 if($f->isSubmitted())
 {
+   
     $em->persist($Event1);
     $em->flush();
 }
@@ -38,4 +41,22 @@ if($f->isSubmitted())
             'f'=>$f,
         ]);
     }
+  
+    
+    #[Route('/appshowdbevent', name: 'app_showauthordb')]
+    public function showdb(EventRepository $event145): Response
+    {
+        $name=$event145->findAll();
+        //dd($name);
+       // var_dump($name).die();
+       // print_r($name);
+       // $nigffger=$author145->find
+        return $this->render('event/showevents.html.twig', [
+            'controller_name' => 'AZIZ',
+            'base_events'=>$name,
+            
+       ]);
+    }
+   
+
 }
