a28d5760e2f92a87be1a49fa4de4cdb1726665a3
Aziz <kraiem.medaziz@esprit.tn>
2024-03-02 12:27:09 +0100

    started the add of ticket and form ticket is done as well

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -5,7 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Ticket;
+use App\Repository\EventRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\TicketType;
 class TicketController extends AbstractController
 {
     #[Route('/ticket', name: 'app_ticket')]
@@ -13,6 +17,33 @@ class TicketController extends AbstractController
     {
         return $this->render('ticket/index.html.twig', [
             'controller_name' => 'TicketController',
+        ]);
+    }
+    #[Route('/appaddTiddcket/{id}', name: 'app_addticddket')]
+    public function addtiddcket(EventRepository $event145,$id,Request $request,ManagerRegistry $em): Response
+    {   $event = $event145->find($id);
+        $em1=$em->getManager();
+        $ticket = new Ticket();
+        $ticket->setEvent($event);
+        $form = $this->createForm(TicketType::class, $ticket, [
+            'event' => $event,
+        ]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $type = $ticket->getType();
+            $type = $ticket->getType();
+        if ($type === 'VIP') {
+            $ticket->setPrice(35);
+        } elseif ($type === 'Standard') {
+            $ticket->setPrice(20);
+        }
+        $em1->persist($ticket);
+        $em1->flush();
+        return $this->redirectToRoute('app_showauthordb');
+        }
+       
+        return $this->render('ticket/form.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/TicketType.php
+++ src/Form/TicketType.php
@@ -6,16 +6,77 @@ use App\Entity\Ticket;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 class TicketType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $event = $options['event'];
+        
         $builder
-            ->add('type')
-            ->add('price')
-            ->add('event')
-            ->add('user')
+            ->add('eventNom', TextType::class, [
+                'data' => $event->getNom(),
+                'mapped' => false,
+                'disabled' => true,
+            ])
+            ->add('eventLieu', TextType::class, [
+                'data' => $event->getLieu(),
+                'mapped' => false,
+                'disabled' => true,
+            ])
+            ->add('eventDate', TextType::class, [
+                'data' => $event->getDateEvent() ? $event->getDateEvent()->format('Y-m-d') : null,
+                'mapped' => false,
+                'disabled' => true,
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'VIP' => 'VIP',
+                    'Standard' => ' standard',
+                ],
+                'placeholder' => 'Select a type',
+                'required' => true,
+            ])
+
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $form = $event->getForm();
+                $data = $event->getData();
+                
+                $typeOptions = [
+                    'choices' => [
+                        'VIP' => 'VIP',
+                        'Standard' => 'Standard',
+                    ],
+                    'placeholder' => 'Select a type',
+                    'required' => true,
+                ];
+            
+                if ($data instanceof Ticket) {
+                    if ($data->getType() === 'VIP') {
+                        $price = 35;
+                    } elseif ($data->getType() === 'Standard') {
+                        $price = 20;
+                    } else {
+                        $price = null; // Set a default value if neither 'VIP' nor 'Standard'
+                    }
+                } else {
+                    $price = null; // Set a default value if $data is not an instance of Ticket
+                }
+            
+                $form
+                    ->add('type', ChoiceType::class, $typeOptions)
+                    ->add('price', NumberType::class, [
+                        'data' => $price,
+                        'required' => true,
+                        'disabled' => true,
+                    ]);
+            })
+            ->add('save', SubmitType::class)
         ;
     }
 
@@ -24,5 +85,6 @@ class TicketType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Ticket::class,
         ]);
+        $resolver->setRequired('event'); // Requires the 'event' option to be set
     }
 }
