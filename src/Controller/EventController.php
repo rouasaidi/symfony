20a93b1c933f4d8fb137c8929592b31f30dafff2
Aziz <kraiem.medaziz@esprit.tn>
2024-03-02 12:20:25 +0100

    i ended the entity event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -30,7 +30,7 @@ class EventController extends AbstractController
 $em=$em->getManager();
 $f=$this->createForm(EventType::class,$Event1);
 $f->handleRequest($req);
-if($f->isSubmitted())
+if($f->isSubmitted() and $f->isValid())
 {
    
     $em->persist($Event1);
@@ -57,6 +57,58 @@ if($f->isSubmitted())
             
        ]);
     }
-   
+    #[Route('/appdeleteevent/{id}', name: 'app_delete')]
+    public function delete(EventRepository $repo,$id,ManagerRegistry $manager): Response
+    {
+         $emm = $manager->getManager();
+            $idremove = $repo->find($id);
+            $emm->remove($idremove);
+            $emm->flush();
+        return $this->redirectToRoute('appback_showeventdb');
+        }
+            
 
+
+
+#[Route('/backshowdbevent', name: 'appback_showeventdb')]
+    public function showback_event(EventRepository $event145): Response
+    {
+        $name=$event145->findAll();
+        //dd($name);
+       // var_dump($name).die();
+       // print_r($name);
+       // $nigffger=$author145->find
+        return $this->render('event/showback.html.twig', [
+            'controller_name' => 'AZIZ',
+            'base_events'=>$name,
+            
+       ]);
+    }
+
+    #[Route('/modifierevent/{id}', name: 'modifierevent')]
+    public function modifierauthor($id,EventType $form,EventRepository $event123, ManagerRegistry $em,Request $req): Response
+    {
+//var_dump($id).die();
+$em=$em->getmanager();
+
+        $namkde= $event123->find($id);
+        //var_dump($namkde).die();
+       
+        //$author=new Authorentity();
+        $form = $this->createForm(EventType::class, $namkde);
+        $form->handleRequest($req);
+       
+       if($form->isSubmitted() and $form->isValid())
+       {
+       $em->persist($namkde);
+    $em->flush();
+    return $this->redirectToRoute('appback_showeventdb');
+       }
+       return $this->renderForm('event/modifyevent.html.twig', [
+        'controller_name' => 'EventController',
+        'f'=>$form,
+    ]);
+
+
+    }
 }
